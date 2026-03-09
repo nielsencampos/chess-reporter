d349bd98e58a2d8a23ff01e1679b5b03ad9108d6
nielsencampos <nielsenmendescampos@hotmail.com>
2026-03-08 21:25:15 -0300

    Implementing chess engine instances for paralelism

---
+++ src/chess_reporter/chess_engine/chess_engine_instance.py
@@ -0,0 +1,111 @@
+"""
+Chess engine instance for the Chess Reporter application.
+"""
+
+from __future__ import annotations
+
+from datetime import datetime, timezone
+from typing import Optional
+
+from chess import Board
+from chess.engine import INFO_SCORE, InfoDict, Limit, PovScore, Score, SimpleEngine
+
+from chess_reporter.chess_domain.chess_domain import ScoreType
+from chess_reporter.chess_engine.chess_engine_domain import (
+    ChessEngineData,
+    EnginePositionAnalysisResult,
+)
+from chess_reporter.chess_engine.chess_engine_parameters import ChessEngineParameters
+
+
+class ChessEngineInstance:
+    """
+    Provide the chess engine instance.
+
+    Methods:
+        get_engine_position_analysis_results: Retrieves the chess engine analysis
+            results for a given chess position.
+        close: Closes the chess engine process and releases any associated resources.
+    """
+
+    def __init__(self) -> None:
+        """
+        Initializes the ChessEngineInstance.
+        """
+        self.__parameters: ChessEngineParameters = ChessEngineParameters()
+        self.__simple_engine: SimpleEngine = SimpleEngine.popen_uci(self.__parameters.path)
+        self.__simple_engine.configure(
+            {"Threads": self.__parameters.threads, "Hash": self.__parameters.hash_table_mb}
+        )
+        self.data: ChessEngineData = ChessEngineData(
+            name=self.__simple_engine.id.get("name", "Unknown Chess Engine"),
+            threads=self.__parameters.threads,
+            hash_table_mb=self.__parameters.hash_table_mb,
+            depth=self.__parameters.depth,
+            evaluation_runs=self.__parameters.evaluation_runs,
+        )
+        self.limit: Limit = Limit(depth=self.__parameters.depth)
+
+    def get_engine_position_analysis_result(
+        self, position_analysis_index: int, board: Board
+    ) -> EnginePositionAnalysisResult:
+        """
+        Retrieves the chess engine analysis result for a given chess position and analysis index.
+
+        Args:
+            position_analysis_index (int): The index of the analysis for the position.
+            board (Board): The chess position for which to retrieve the analysis result.
+
+        Returns:
+            EnginePositionAnalysisResult: The chess engine analysis result for the given
+                position and analysis index.
+        """
+        if not isinstance(position_analysis_index, int) or position_analysis_index < 1:
+            raise ValueError("position_analysis_index must be a positive integer.")
+
+        if not isinstance(board, Board):
+            raise TypeError(f"Expected board to be an instance of chess.Board, got {type(board)}")
+
+        started_analysis_at: datetime = datetime.now(timezone.utc)
+        pre_engine_position_analysis_result: InfoDict = self.__simple_engine.analyse(
+            board=board.copy(stack=False), limit=self.limit, info=INFO_SCORE
+        )
+        pre_pov_score: Optional[PovScore] = pre_engine_position_analysis_result.get("score")
+
+        if not isinstance(pre_pov_score, PovScore):
+            raise ValueError("Engine analysis did not return a valid score.")
+
+        pov_score: PovScore = pre_pov_score
+        score: Score = pov_score.white()
+        score_type: ScoreType = ScoreType.MATE if pov_score.is_mate() else ScoreType.CP
+        pre_score_value: Optional[int] = score.mate() if pov_score.is_mate() else score.score()
+
+        if not isinstance(pre_score_value, int):
+            raise ValueError("Engine analysis did not return a valid score value.")
+
+        score_value: int = pre_score_value
+        depth: int = pre_engine_position_analysis_result.get("depth", self.__parameters.depth)
+        seldepth: int = pre_engine_position_analysis_result.get("seldepth", self.__parameters.depth)
+        time_in_seconds: float = pre_engine_position_analysis_result.get("time", 0.0)
+        ended_analysis_at: datetime = datetime.now(timezone.utc)
+        engine_position_analysis_result: EnginePositionAnalysisResult = (
+            EnginePositionAnalysisResult(
+                position_analysis_index=position_analysis_index,
+                score_type=score_type,
+                score_value=score_value,
+                depth=depth,
+                seldepth=seldepth,
+                time_in_seconds=time_in_seconds,
+                is_forced_result=False,
+                started_analysis_at=started_analysis_at,
+                finished_analysis_at=ended_analysis_at,
+            )
+        )
+
+        return engine_position_analysis_result
+
+    def close(self) -> None:
+        """
+        Closes the chess engine process and releases any associated resources.
+        """
+        self.__simple_engine.quit()
--- src/chess_reporter/chess_engine/chess_engine_manager.py
+++ src/chess_reporter/chess_engine/chess_engine_manager.py
@@ -4,17 +4,19 @@ Chess engine manager for the Chess Reporter application.
 
 from __future__ import annotations
 
+from asyncio import AbstractEventLoop, gather, get_event_loop, run
+from concurrent.futures import ThreadPoolExecutor
 from datetime import datetime, timezone
-from typing import List, Optional
+from typing import List
 
 from chess import Board
-from chess.engine import INFO_SCORE, InfoDict, Limit, PovScore, Score, SimpleEngine
 
 from chess_reporter.chess_domain.chess_domain import ResultType, ScoreType
 from chess_reporter.chess_engine.chess_engine_domain import (
     ChessEngineData,
     EnginePositionAnalysisResult,
 )
+from chess_reporter.chess_engine.chess_engine_instance import ChessEngineInstance
 from chess_reporter.chess_engine.chess_engine_parameters import ChessEngineParameters
 from chess_reporter.database.database_domain import Query
 from chess_reporter.database.database_manager import DatabaseManager
@@ -35,18 +37,14 @@ class ChessEngineManager:
         Initializes the ChessEngineManager.
         """
         self.parameters: ChessEngineParameters = ChessEngineParameters()
-        self.simple_engine: SimpleEngine = SimpleEngine.popen_uci(self.parameters.path)
-        self.simple_engine.configure(
-            {"Threads": self.parameters.threads, "Hash": self.parameters.hash_table_mb}
+        self.__instances: List[ChessEngineInstance] = [
+            ChessEngineInstance() for _ in range(self.parameters.evaluation_runs)
+        ]
+        self.data: ChessEngineData = self.__instances[0].data
+
+        self.__executor: ThreadPoolExecutor = ThreadPoolExecutor(
+            max_workers=self.parameters.evaluation_runs
         )
-        self.data: ChessEngineData = ChessEngineData(
-            name=self.simple_engine.id.get("name", "Unknown Chess Engine"),
-            threads=self.parameters.threads,
-            hash_table_mb=self.parameters.hash_table_mb,
-            depth=self.parameters.depth,
-            evaluation_runs=self.parameters.evaluation_runs,
-        )
-        self.limit: Limit = Limit(depth=self.parameters.depth)
 
         self.__maintain_data__()
 
@@ -73,63 +71,33 @@ class ChessEngineManager:
         finally:
             database_manager.close()
 
-    def __get_engine_position_analysis_result__(
-        self, position_analysis_index: int, board: Board
-    ) -> EnginePositionAnalysisResult:
+    async def async_get_engine_position_analysis_results(
+        self, board: Board
+    ) -> List[EnginePositionAnalysisResult]:
         """
-        Retrieves the chess engine analysis result for a given chess position and analysis index.
+        Asynchronously retrieves the chess engine analysis results for a given chess position.
 
         Args:
-            position_analysis_index (int): The index of the analysis for the position.
-            board (Board): The chess position for which to retrieve the analysis result.
+            board (Board): The chess position for which to retrieve the analysis results.
+            result (ResultType, optional): The result of the game for the given position.
 
         Returns:
-            EnginePositionAnalysisResult: The chess engine analysis result for the given
-                position and analysis index.
+            List[EnginePositionAnalysisResult]: A list of chess engine analysis
+                results for the given position.
         """
-        if not isinstance(position_analysis_index, int) or position_analysis_index < 1:
-            raise ValueError("position_analysis_index must be a positive integer.")
 
-        if not isinstance(board, Board):
-            raise TypeError(f"Expected board to be an instance of chess.Board, got {type(board)}")
-
-        started_analysis_at: datetime = datetime.now(timezone.utc)
-        pre_engine_position_analysis_result: InfoDict = self.simple_engine.analyse(
-            board=board.copy(stack=False), limit=self.limit, info=INFO_SCORE
-        )
-        pre_pov_score: Optional[PovScore] = pre_engine_position_analysis_result.get("score")
-
-        if not isinstance(pre_pov_score, PovScore):
-            raise ValueError("Engine analysis did not return a valid score.")
-
-        pov_score: PovScore = pre_pov_score
-        score: Score = pov_score.white()
-        score_type: ScoreType = ScoreType.MATE if pov_score.is_mate() else ScoreType.CP
-        pre_score_value: Optional[int] = score.mate() if pov_score.is_mate() else score.score()
-
-        if not isinstance(pre_score_value, int):
-            raise ValueError("Engine analysis did not return a valid score value.")
-
-        score_value: int = pre_score_value
-        depth: int = pre_engine_position_analysis_result.get("depth", self.parameters.depth)
-        seldepth: int = pre_engine_position_analysis_result.get("seldepth", self.parameters.depth)
-        time_in_seconds: float = pre_engine_position_analysis_result.get("time", 0.0)
-        ended_analysis_at: datetime = datetime.now(timezone.utc)
-        engine_position_analysis_result: EnginePositionAnalysisResult = (
-            EnginePositionAnalysisResult(
-                position_analysis_index=position_analysis_index,
-                score_type=score_type,
-                score_value=score_value,
-                depth=depth,
-                seldepth=seldepth,
-                time_in_seconds=time_in_seconds,
-                is_forced_result=False,
-                started_analysis_at=started_analysis_at,
-                finished_analysis_at=ended_analysis_at,
+        loop: AbstractEventLoop = get_event_loop()
+        tasks = [
+            loop.run_in_executor(
+                self.__executor,
+                self.__instances[position_analysis_index - 1].get_engine_position_analysis_result,
+                position_analysis_index,
+                board,
             )
-        )
+            for position_analysis_index in range(1, self.data.evaluation_runs + 1)
+        ]
 
-        return engine_position_analysis_result
+        return await gather(*tasks)
 
     def get_engine_position_analysis_results(
         self, board: Board, result: ResultType = ResultType.ONGOING
@@ -153,9 +121,8 @@ class ChessEngineManager:
 
         engine_position_analysis_results: List[EnginePositionAnalysisResult] = []
 
-        for position_analysis_index in range(1, self.data.evaluation_runs + 1):
-            position_analysis_data: Optional[EnginePositionAnalysisResult] = None
-            if result is not ResultType.ONGOING:
+        if result is not ResultType.ONGOING:
+            for position_analysis_index in range(1, self.data.evaluation_runs + 1):
                 started_analysis_at: datetime = datetime.now(timezone.utc)
                 score_type: ScoreType = ScoreType.MATE if result.has_winner else ScoreType.CP
                 score_value: int = 0
@@ -163,7 +130,7 @@ class ChessEngineManager:
                 seldepth: int = 0
                 time_in_seconds: float = 0.0
                 ended_analysis_at: datetime = datetime.now(timezone.utc)
-                position_analysis_data = EnginePositionAnalysisResult(
+                position_analysis_data: EnginePositionAnalysisResult = EnginePositionAnalysisResult(
                     position_analysis_index=position_analysis_index,
                     score_type=score_type,
                     score_value=score_value,
@@ -174,15 +141,11 @@ class ChessEngineManager:
                     started_analysis_at=started_analysis_at,
                     finished_analysis_at=ended_analysis_at,
                 )
-            else:
-                position_analysis_data = self.__get_engine_position_analysis_result__(
-                    position_analysis_index=position_analysis_index, board=board.copy(stack=False)
-                )
-
-            if position_analysis_data is None:
-                raise ValueError("Failed to retrieve engine position analysis result.")
-
-            engine_position_analysis_results.append(position_analysis_data)
+                engine_position_analysis_results.append(position_analysis_data)
+        else:
+            engine_position_analysis_results = run(
+                self.async_get_engine_position_analysis_results(board)
+            )
 
         if len(engine_position_analysis_results) != self.data.evaluation_runs:
             raise ValueError(
@@ -196,4 +159,7 @@ class ChessEngineManager:
         """
         Closes the chess engine process and releases any associated resources.
         """
-        self.simple_engine.quit()
+        self.__executor.shutdown(wait=True)
+
+        for instance in self.__instances:
+            instance.close()
